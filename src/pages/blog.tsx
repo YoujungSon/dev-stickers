6a0a33692ad17d140ed6a822568cd7c3fe22b0f7
YoujungSon <andn1026@gmail.com>
2022-11-25 17:03:11 +0900

    #10.10 Page Query

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,14 +1,29 @@
+import { graphql } from 'gatsby';
 import React from 'react';
 import Layout from './components/Layout';
 import Seo from './components/Seo';
+import { PageProps } from 'gatsby';
 
-const blog = () => {
+const blog = ({ data }: PageProps<Queries.BlogTitlesQuery>) => {
   return (
-    <Layout title='Hello welcome to my blog!'>
-      <p>the most recent news from my shop</p>
+    <Layout title='Blog'>
+      {data.allFile.nodes.map((file, index) => (
+        <li key={index}>{file.name}</li>
+      ))}
     </Layout>
   );
 };
+
+export const query = graphql`
+  query BlogTitles {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`;
+
 export const Head = () => <Seo title='blog' />;
 
 export default blog;
